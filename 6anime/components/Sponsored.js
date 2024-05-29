bf1c3127b9ca8a11256dbf1c3570f15b26ca3c97
MattZafeiriou <matuaioszafeiriou@gmail.com>
2024-05-30 00:58:09 +0300

    Updated Sponsor text

--- 6anime/components/Sponsored.js
+++ 6anime/components/Sponsored.js
@@ -9,7 +9,7 @@ export default function Sponsored({ data }) {
                     <a href='https://yt2mp3.tv/' target='_blank' rel='nofollow'>
                         <h4>yt2mp3.tv</h4>
                     </a>
-                    <h5>Ads-free, lightning-fast YouTube converter, respecting your privacy. Experience the speed you deserve!</h5>
+                    <h5>Ads-free, lightning-fast audio converter with trimming support. Respects your privacy. Experience the speed you deserve!</h5>
                 </div>
 
                 <h3>Sponsored</h3>
