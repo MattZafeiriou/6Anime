7fa0d006e023395975ea0705013f1a948b5d8e4f
MattZafeiriou <matuaioszafeiriou@gmail.com>
2024-06-04 11:31:50 +0300

    Fixed mysql2 vulnerability, theme secure, and login

---
+++ 6anime/pages/profile.js
@@ -0,0 +1,12 @@
+import { useEffect } from 'react';
+
+export default function Profile({ data }) {
+
+    return (
+        <>
+            <div className='container'>
+                <h1>My Profile</h1>
+            </div>
+        </>
+    );
+}
