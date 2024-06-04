800c15d989ec37c37400186c06083a00d0ef9805
MattZafeiriou <matuaioszafeiriou@gmail.com>
2024-06-04 20:05:13 +0300

    Fixed bugs on automatically adding anime

--- 6anime/lib/repeatTasks.js
+++ 6anime/lib/repeatTasks.js
@@ -3,7 +3,7 @@ const sqlHandler = require('./sqlHandler')
 const backupHandler = require("./utils/backupHandler");
 const getAnimeInfo = require('./utils/getAnimeInfo.js');
 const addvideo = require('./routes/addvideo.js');
-import { META } from "@consumet/extensions"
+const { META } = require("@consumet/extensions");
 /*
     This was done with the help of https://crontab.guru
 */
@@ -67,7 +67,9 @@ async function getTodayMostUsedTag() {
 
 // This task repeats every day at 00:00
 function repeatDaily() {
+  autoAddAnime();
   schedule.scheduleJob('0 0 * * *', async () => {
+    autoAddAnime();
     // Delete all daily views
     await getTodayMostUsedTag();
     sqlHandler.con.query('UPDATE Views SET today_views = 0 WHERE today_views > 0;', (err, result) => {
@@ -79,10 +81,8 @@ function repeatDaily() {
 
 // This task repeats every Monday at 00:00
 function repeatWeekly() {
-  autoAddAnime();
   schedule.scheduleJob('0 0 * * 1', async () => {
     // Create a backup of the database
-    autoAddAnime();
     backupHandler.backup();
     console.log('Database backup created');
 
@@ -158,43 +158,40 @@ async function updateAnime() {
   }) // every 12 hours
 }
 
-async function autoAddAnime() {
+async function autoAddAnime(page = 1) {
   const anilist = new META.Anilist();
   const today = new Date();
   const now = today.getTime().toString().slice(0, -3)
-  const week = 604800;
-  let page = 1;
-  let hasNextPage = true;
+  //const week = 604800;
+  const day = 86400;
 
-  while (hasNextPage){
-    await anilist.fetchAiringSchedule(page, 20, Number(now), Number(now) + week, true).then(data => {
-      hasNextPage = data.hasNextPage;
-      page++;
 
-      const results = data.results;
-      const time = data.airingAt - now;
-      for (let i = 0; i < results.length; i++)
-        {
-          setTimeout(async () => {
-            const anime = results[i];
-            const anime_id = anime.id;
-            const animeInfo = await getAnimeInfo_(anime_id);
-            if (animeInfo === undefined)
-              {
-                addvideo.addAnime(anime_id)
-              } else {
-                const episode = anime.episode;
-                sqlHandler.con.query(`UPDATE Anime SET episodes = ${episode}, update_date = CURRENT_DATE WHERE id = ${animeInfo.id};`, (err, result) => {
-                  if (err) throw err
-                })
-              }
+  await anilist.fetchAiringSchedule(page, 20, Number(now), Number(now) + day, true).then(data => {
+    if (data.hasNextPage) {
+      autoAddAnime(page + 1);
+    }
 
-          }, 1000 * (time + 60)); // add 60 seconds to the time
+    const results = data.results;
+    for (let i = 0; i < results.length; i++) {
+      const time = Number(results[i].airingAt) - now;
+      setTimeout(async () => {
+        const anime = results[i];
+        const anime_id = anime.id;
+        const animeInfo = await getAnimeInfo_(anime_id);
+        if (animeInfo === undefined) {
+          addvideo.addAnime(anime_id)
+        } else {
+          const episode = anime.episode;
+          sqlHandler.con.query(`UPDATE Anime SET episodes = ${episode}, update_date = CURRENT_DATE WHERE id = ${animeInfo.id};`, (err, result) => {
+            if (err) throw err
+          })
         }
-    }).catch(e => {
-      console.log(e);
-    });
-  }
+
+      }, 1000 * (time + 60)); // add 60 seconds to the time
+    }
+  }).catch(e => {
+    console.log(e);
+  });
 }
 
 module.exports = { repeatDaily, repeatWeekly, repeatMonthly, repeatYearly, todayTag, updateAnime, autoAddAnime };
